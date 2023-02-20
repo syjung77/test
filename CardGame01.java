3cd1e8cfaa67eb01c8f431f032d81165447f6c13
syjung <syjung7777@gmail.com>
2023-02-20 15:32:06 +0900

    Create CardGame01.java

---
+++ CardGame01.java
@@ -0,0 +1,19 @@
+package cardGame;
+
+public class CardGame01 {
+
+	public static void main(String[] args) {
+		
+		// loop로 개선
+		// 첫번째 카드
+		int i = (int) (Math.random() * 4 + 1);
+		int j = (int) (Math.random() * 13 + 1);
+		System.out.println("1카드는 " + i + "모양의 " + j+"번째 카드");
+		
+		// 두번째 카드
+		int i1 = (int) (Math.random() * 4 + 1);
+		int j1 = (int) (Math.random() * 13 + 1);
+		System.out.println("2카드는 " + i1 + "모양의 " + j1+"번째 카드");
+
+	}
+}
